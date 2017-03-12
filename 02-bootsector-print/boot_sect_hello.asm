1ce740fccdbdf10a31f8c5cd1ba6c69fac4bdc04
Aruna Balasubramanian <aruna.balavijay@gmail.com>
2017-03-12 14:41:30 +1100

    Update boot_sect_hello.asm

--- 02-bootsector-print/boot_sect_hello.asm
+++ 02-bootsector-print/boot_sect_hello.asm
@@ -1,16 +1,25 @@
-mov ah, 0x0e ; tty mode
+;
+; A simple boot sector that prints a message to the screen using a BIOS routine.
+;
+
+mov ah, 0x0e    ; int 10/ ah = 0eh -> scrolling teletype BIOS routine
+
 mov al, 'H'
 int 0x10
 mov al, 'e'
 int 0x10
 mov al, 'l'
 int 0x10
-int 0x10 ; 'l' is still on al, remember?
+int 0x10        ; 'l' is still on al, remember?
 mov al, 'o'
 int 0x10
 
-jmp $ ; jump to current address = infinite loop
+jmp $           ; jump to current address ( i.e. forever ) = infinite loop
 
-; padding and magic number
-times 510 - ($-$$) db 0
-dw 0xaa55 
+;
+; Padding and magic BIOS number.
+;
+
+times 510 - ($-$$) db 0   ; Pad the boot sector out with zeros
+dw 0xaa55                 ; Last two bytes form the magic number ,
+                          ; so BIOS knows we are a boot sector.
